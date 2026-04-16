8397133d55ffc7e72ab7cffa3fa1fed6038c394f
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2026-04-16 16:58:49 +0530

    done

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+  origin: "*"
+}
+));
 app.use(express.json());
 app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URI)
@@ -27,6 +31,7 @@ app.use('/api/purchases', purchaseRoutes);
 app.use('/api/transfers', transferRoutes);
 app.use('/api/assignments', assignmentRoutes);
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
 });
